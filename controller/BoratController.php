6b129724a56c07128052d25a94a5c7740af4a529
jonathan <bitbucket@jmartz.de>
2019-10-25 21:13:50 +0200

    add logic for packages

--- controller/BoratController.php
+++ controller/BoratController.php
@@ -9,11 +9,45 @@ use \Illuminate\Support\Facades\Hash;
 
 class BoratController extends Controller
 {
-    public function packages(){
-        return 'packages';
+    public function packages(Request $request){
+        $packages = DB::table('packages');
+
+        $json = [
+            'providers' => [],
+            'mirrors' => [
+                'dist-url' => 'https://api-borat.jmartz.de/dists/%package%/%version%/%reference%.%type%',
+                'preferred' => true
+            ],
+            'providers-url' => '/p/%package%.json'
+        ];
+
+        foreach ($packages->get() as $key => $value){
+            $json['providers'][$value->fullname] = [
+                'sha256' => null
+            ];
+        }
+
+        return response()->json([
+            $json
+        ]);
     }
 
-    public function package(){
-        return 'package';
+    public function package(Request $request){
+        $routeInfo = $request->route();
+        // var_dump($routeInfo[2]['vendor']);
+        // var_dump($routeInfo[2]['module']);
+
+        $this->cloneRepo();
+        $this->listVersions();
+
     }
+
+    public function cloneRepo(){
+
+    }
+
+    public function listVersions(){
+
+    }
+
 }

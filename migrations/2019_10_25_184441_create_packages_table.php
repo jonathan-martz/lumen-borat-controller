895eaf3ba4abbf7134569b36112d5d2088cd85d4
jonathan <bitbucket@jmartz.de>
2019-10-26 12:43:07 +0200

    add migration for packages

---
+++ migrations/2019_10_25_184441_create_packages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('vendor');
+            $table->string('module');
+            $table->string('fullname');
+            $table->string('repo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}

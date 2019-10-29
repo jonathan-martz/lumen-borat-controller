15554ed5f6255537f3152c3829e6dc69050cff69
jonathan <bitbucket@jmartz.de>
2019-10-29 08:13:04 +0100

    remove requirement for lumen-controller

--- controller/BoratController.php
+++ controller/BoratController.php
@@ -7,6 +7,7 @@ use \Illuminate\Http\Request;
 use \Illuminate\Support\Facades\DB;
 use \Illuminate\Support\Facades\Hash;
 
+
 class BoratController extends Controller
 {
     public function packages(Request $request){

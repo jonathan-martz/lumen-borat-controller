526a13db1925f38481964714652b454db5607286
jonathan-martz <github@jmartz.de>
2019-12-28 21:42:03 +0100

    add provider for routes and migrations

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::post('{type}/p/{vendor}/{module}.json', [
+    'middleware' => ['xss', 'https'],
+    'uses' => 'BoratController@package'
+]);
+
+Route::get('{type}/packages.json', [
+    'middleware' => ['xss', 'https'],
+    'uses' => 'App\Http\Controllers\BoratController@packages'
+]);
+
+Route::get('{type}/p/{vendor}/{module}.json', [
+    'middleware' => ['xss', 'https'],
+    'uses' => 'App\Http\Controllers\BoratController@package'
+]);
+
+Route::get('{type}/packages.json', [
+    'middleware' => ['xss', 'https'],
+    'uses' => 'App\Http\Controllers\BoratController@packages'
+]);
